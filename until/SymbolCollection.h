2efe6fddef227d69de8a9823f9ce27faae8963d5
DongMao <1520526911@qq.com>
2022-06-28 22:04:29 +0800

    增加终结符

--- until/SymbolCollection.h
+++ until/SymbolCollection.h
@@ -2,11 +2,11 @@
 #include <set>
 
 const std::set<char> Terminator{
-	'a','b','c','d','e','%','g','f'
+	'a','b','c','d','e','f','g','h','i','g','k','l','%'
 };
 
 const std::set<char> UnTerminator{
-	'A','B','C','D','E','F'
+	'A','B','C','D','E','F','G','H','I','G','K','L'
 };
 
 //判断该符号是否为终结符
